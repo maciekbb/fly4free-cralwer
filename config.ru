bc84aa570bdaa2cdefac38efaa6d0bc51ab6fae4
Maciej Wątroba <maciej.watroba@getbase.com>
2015-11-01 17:33:59 +0100

    remove controllers folder

--- config.ru
+++ config.ru
@@ -1,10 +1,3 @@
-["crawlers", "entities", "parsers", "persistance"].each do |dir|
-  Dir[File.dirname(__FILE__) + "/../#{dir}/*.rb"].each do |file|
-    puts "#{__FILE__} Require #{file}"
-    require file
-  end
-end
-
-require './app/controllers/application_controller'
+require './main'
 
 run Sinatra::Application
--- main.rb
+++ main.rb
@@ -1,17 +1,18 @@
-require 'pry'
-require 'redis'
-
-require_relative 'config/initializers/redis'
+require 'sinatra'
 
 ["crawlers", "entities", "parsers", "persistance"].each do |dir|
   Dir[File.dirname(__FILE__) + "/app/#{dir}/*.rb"].each do |file|
+    puts "#{__FILE__} Require #{file}"
     require file
   end
 end
 
-page_content = Crawlers::MainPage.new(1).fetch
+get '/crawl/:page' do
+  page_content = Crawlers::MainPage.new(params[:page]).fetch
+  Parsers::MainPage.new(page_content).analyze.to_json
+end
 
-parsed = Parsers::MainPage.new(page_content).analyze
-article_content = Crawlers::ArticlePage.from_url(parsed.entries[1][:url]).fetch
-parsed_article = Parsers::ArticlePage.new(article_content).analyze
-binding.pry
+get '/crawl/article/:id' do
+  article_content = Crawlers::ArticlePage.new(id).fetch
+  Parsers::ArticlePage.new(article_content).analyze.to_json
+end
